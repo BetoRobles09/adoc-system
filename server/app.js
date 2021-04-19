e090203c20ec0229ff48e8a295c4c8a7e267ba86
Alberto Robles <beto.robles25@gmail.com>
2021-04-19 13:59:40 -0500

    Agregue dependencias y env variables para seguridad

--- server/app.js
+++ server/app.js
@@ -1,13 +1,29 @@
 const express = require('express');
-const connectDB = require('./config/db');
+const conectarDB = require('./config/db');
+const cors = require('cors');
 
+// crear el servidor
 const app = express();
 
-// Connect Database
-connectDB();
+// Conectar a la base de datos
+conectarDB();
 
-app.get('/', (req, res) => res.send('Hello world!'));
+// habilitar cors
+app.use(cors());
 
-const port = process.env.PORT || 8082;
+// Habilitar express.json
+app.use( express.json({ extended: true }));
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// puerto de la app
+const port = process.env.PORT || 4000;
+
+// Importar rutas
+/* app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/proyectos', require('./routes/proyectos'));
+app.use('/api/tareas', require('./routes/tareas')); */
+
+// arrancar la app
+app.listen(port, '0.0.0.0', () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
+});
--- server/config/db.js
+++ server/config/db.js
@@ -1,22 +1,19 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+require('dotenv').config({ path: 'variables.env' });
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(
-      db,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }
-    );
+const conectarDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_MONGO, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        });
+        console.log('DB Conectada');
+    } catch (error) {
+        console.log('hubo un error')
+        console.log(error);
+        process.exit(1); // Detener la app
+    }
+}
 
-    console.log('MongoDB is Connected...');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-};
-
-module.exports = connectDB;
+module.exports = conectarDB;
